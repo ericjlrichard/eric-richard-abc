35818b6916a3c90106941c0f4e91556fe54693f5
Eric JL Richard <eric.jl.richard@gmail.com>
2022-10-26 17:22:19 -0400

    init

---
+++ src/components/HamburgerMenu/HamburgerMenu.js
@@ -0,0 +1,17 @@
+import "./HamburgerMenu.scss"
+import { Link, NavLink } from "react-router-dom";
+
+
+export default function HamburgerMenu({showHamburger}) {
+
+  if (!showHamburger) {
+    return null;
+  }
+
+  return (
+    <div className="hamburger__menu">
+      <NavLink className="hamburger__link" to="/train">Train</NavLink>
+      <NavLink className="hamburger__link" to="/account">Account</NavLink>
+    </div>
+  )
+}
--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,14 +1,30 @@
 
 import "./MainHeader.scss"
+import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
+import hamburgerIcon from "../../assets/icons/hamburger-menu.png"
+import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
 
 export default function MainHeader() {
+  const [showHamburger, setShowHamburger] = useState(false);
 
-
+  const handleClickHamburger= (event) => {
+    setShowHamburger(!showHamburger)
+  }
 
   return (
     <header className="header_menu">
-      <div className="header_menu__logo">This is Angelo's logo</div>
-      <div className="header_menu__hamburger">Hamb!</div>
+      <HamburgerMenu showHamburger={showHamburger} />
+      <nav className="header_menu__nav">
+
+        <Link to="/main"><div className="header_menu__logo">Logo</div></Link>
+        <div className="header_menu__bar">
+          <NavLink to="/train"><div className="header_menu__link">Train</div></NavLink>
+          <NavLink to="/account"><div className="header_menu__link">Account</div></NavLink>
+        </div>
+        <img className="header_menu__hamburger-icon" alt="Top Right Menu" src={hamburgerIcon} onClick={handleClickHamburger}/>
+      </nav>
+      
     </header>
   )
 }
