f9b7080fb8b3b8c1ed06ea8b64b58b9ef6f871de
Eric JL Richard <eric.jl.richard@gmail.com>
2022-11-01 11:15:38 -0400

    cleaned up sounds

--- src/js/sound-utils.js
+++ src/js/sound-utils.js
@@ -1,10 +1,5 @@
 
 
-exports.playSound = () => {
-  const audio = new Audio("http://localhost:8080/sounds/hit3.wav");
-  audio.play();
-}
-
 exports.setSoundTimer = (ms, action) => {
   console.log(action)
   setTimeout(() => {
