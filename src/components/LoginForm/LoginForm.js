6c13ce1407b403349e4953399ae0de39d586969e
Eric JL Richard <eric.jl.richard@gmail.com>
2022-11-25 01:05:10 -0500

    changed size and style of actions to make them more visible from afar

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,16 +1,14 @@
 
 
-export default function LoginForm({handleFormSubmit}) {
+export default function SignUpForm({handleLoginSubmit}) {
 
 
   return (
     <div className="account__form-container">
-      <form id="signup-form" className="account__signup-form" onSubmit={handleFormSubmit} >
-        <label htmlFor="email">Handle: </label><input name="handle" id="handle" type="text" required></input>
-        <label htmlFor="handle">Email: </label><input name="email" id="email" type="email" required></input>
-        <label htmlFor="password">Password: </label><input name="password" id="password" type="password" required></input>
-        <label htmlFor="password">Confirm Password: </label><input name="confirm-password" id="confirm-password" type="password" required></input>
-        <button className="account__button">Register</button>
+      <form id="login-form" className="account__signup-form" onSubmit={handleLoginSubmit} >
+        <label htmlFor="login_email">Handle: </label><input name="login_handle" id="login_handle" type="text" required></input>
+        <label htmlFor="login_password">Password: </label><input name="login_password" id="login_password" type="password" required></input>
+        <button className="account__button">Log In</button>
       </form>
     </div>
   )
